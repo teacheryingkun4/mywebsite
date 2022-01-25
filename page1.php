4240e9a0077446526ea417c4aaa285bda33e9f02
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-01-25 13:53:30 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -3,7 +3,6 @@
                 <title>งานที่ 1</title>
         </head>
         <body bgcolor="lightblue">
-                <p text="orange">ข้อความที่ 1</p>
-                <p text="gray">ข้อความที่ 2</p>
+                <b>ข้อความ : </b> <i> ข้อความ </i>
         </body>
 </html>
