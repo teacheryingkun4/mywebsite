a38bd46aed2a0bae51054f1f254be3117f4da714
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-01-25 13:39:23 +0700

    Update page1.php

--- page1.php
+++ page1.php
@@ -3,6 +3,7 @@
                 <title>งานที่ 1</title>
         </head>
         <body bgcolor="lightblue">
-                page1ธรรมดา - - - <b>page1 หนา</b>
+                page1ธรรมดา 
+                <b>page1 หนา</b>
         </body>
 </html>
