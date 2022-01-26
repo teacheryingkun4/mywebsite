47fe9e6e4969d7c562a0cf8faff8669b15f4ed90
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-01-26 12:46:47 +0700

    Delete page1.php

--- page1.php
+++
@@ -1,8 +0,0 @@
-<html>
-        <head>
-                <title>งานที่ 1</title>
-        </head>
-        <body bgcolor="lightblue">
-                <p style="color:red;"> <b>ข้อความ : </b> <i> ข้อความ </i> </p>
-        </body>
-</html>
