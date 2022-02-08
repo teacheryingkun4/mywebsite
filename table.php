d7414961c2d0c82417c15d06aa8a6112fcc01a1d
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-08 14:25:45 +0700

    Update table.php

--- table.php
+++ table.php
@@ -5,9 +5,15 @@
   <body>
     <table border="1">
       <tr>
-        <th>คอลัมน์ที่ 1</th>
-        <td>คอลัมน์ที่ 2</td>
+        <th>ลำดับ</th>
+        <th>ชื่อ - นามสกุล</th>
+        <th>ชื่อเล่น</th>
       </tr>
+      <tr>
+        <td>1</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td>ขม</td>
+      </tr>  
     </table>
   </body> 
 </html>
