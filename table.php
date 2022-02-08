6da4aabc257fab5f5c286f54e2cbfeacf496507a
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-08 14:16:18 +0700

    Create table.php

---
+++ table.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>ตารางของฉัน</title>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td>คอลัมน์ที่ 1</td>
+        <td>คอลัมน์ที่ 2</td>
+      </tr>
+    </table>
+  </body> 
+</html>
