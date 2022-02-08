c25126f0f1f5f35ccd608e8a0b1ebe0cc80c372c
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-08 14:31:17 +0700

    Update table.php

--- table.php
+++ table.php
@@ -9,12 +9,14 @@
         <th>ชื่อ - นามสกุล</th>
         <th>ชื่อเล่น</th>
         <th>รูป</th>
+        <th>ทวิตเตอร์</th>
       </tr>
       <tr>
         <td>1</td>
         <td>ยิ่งคุณ รอดทิม</td>
         <td>ขม</td>
         <td><img src="https://asianwiki.com/images/e/e8/Song_Joong-Ki-p002.jpeg" width="130px"></td>
+        <td><a href="https://twitter.com/sjk_kith?lang=en">twitter</a></td>
       </tr>  
     </table>
   </body> 
