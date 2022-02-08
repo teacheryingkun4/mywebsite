06e3e3b137412319371513b6f7e7ee8329b8f193
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-08 14:28:31 +0700

    Update table.php

--- table.php
+++ table.php
@@ -8,11 +8,13 @@
         <th>ลำดับ</th>
         <th>ชื่อ - นามสกุล</th>
         <th>ชื่อเล่น</th>
+        <th>รูป</th>
       </tr>
       <tr>
         <td>1</td>
         <td>ยิ่งคุณ รอดทิม</td>
         <td>ขม</td>
+        <td><img src="https://asianwiki.com/images/e/e8/Song_Joong-Ki-p002.jpeg" width="130px"></td>
       </tr>  
     </table>
   </body> 
