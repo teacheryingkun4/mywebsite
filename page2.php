4696a551f3e4792bb4c6f26b1a97556ed31a3bc9
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-15 13:34:56 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -2,16 +2,16 @@
 	<head>
 		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
 		<style>
-			#text1{
+			.text1{
 				color:green;
 			}
-			#text2{
+			.text2{
 				color:yellow;
 			}
 		</style>
 	</head>
 	<body>
-		<h1 id="text1"> ข้อความนี้สีเขียว H1 </h1>
-		<h1 id="text2"> ข้อความนี้สีเหลือง H1 </h1>
+		<h1 class="text1"> ข้อความนี้สีเขียว H1 </h1>
+		<h1 class="text2"> ข้อความนี้สีเหลือง H1 </h1>
 	</body>
 </html>
