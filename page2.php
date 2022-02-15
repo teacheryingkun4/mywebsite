094dbfa95dfe3b70fa881401a960b03848ef0bb0
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-15 13:44:16 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -2,11 +2,15 @@
 	<head>
 		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
 		<style>
-			
+			#box1{
+				width: 800px;
+				height: 350px;
+				background-color: lightblue;
+			}
 		</style>
 	</head>
 	<body>
-		<div>
+		<div id="box1">
 			<h1>กล่องของฉัน</h1>
 		</div>
 	</body>
