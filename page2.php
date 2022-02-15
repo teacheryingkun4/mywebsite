496d83fa9f7bf98961b02093ee2424204615f26c
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-15 13:48:22 +0700

    Update page2.php

--- page2.php
+++ page2.php
@@ -7,11 +7,19 @@
 				height: 350px;
 				background-color: lightblue;
 			}
+			#box2{
+				width: 600px;
+				height: 150px;
+				background-color: red;
+			}
 		</style>
 	</head>
 	<body>
 		<div id="box1">
-			<h1>กล่องของฉัน</h1>
+			<h1>กล่องที่ 1</h1>
+		</div>
+		<div id="box2">
+			<h1>กล่องที่ 2</h1>
 		</div>
 	</body>
 </html>
