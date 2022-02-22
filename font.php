f0afc0f2e71ef95a3d81bd84dfe69b3f64740586
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-22 13:18:09 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    @font-face{
+      font-family: myFont1;
+      src: url(SOV_sorm2496.woff)
+    }
+  </style>
+</head>
+<body>
+  <h1>เว็บไซต์ของฉัน</h1>
+</body>
+</html>
