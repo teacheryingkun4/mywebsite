1e030ea351690b6603243497de8c319a7e76d265
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-22 13:23:03 +0700

    Update font.php

--- font.php
+++ font.php
@@ -8,9 +8,13 @@
       font-family: myFont1;
       src: url(SOV_sorm2496.woff)
     }
+    #font1{
+	font-family: myFont1;    
+    }	  
   </style>
 </head>
 <body>
-  <h1>เว็บไซต์ของฉัน</h1>
+  <h1>ฟอนต์เดิม</h1>
+  <h1 id="font1">ฟอนต์ใหม่</h1>
 </body>
 </html>
