cb4bf099563f46f19ceb6ce8790d25a382b1dcaa
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-23 13:14:31 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>การกำหนดรูปแบบตัวอักษร</title>
+    <style>
+      @font-face{
+        font-family: "MyFont1";
+        src: url("baozi.ttf");
+      }
+      body{
+        font-family: "MyFont1";
+      }
+    </style>
+  </head>
+  <body>
+    <h1>ฟอนต์ของฉัน</h1>
+  </body>
+</html>
