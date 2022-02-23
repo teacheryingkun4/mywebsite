c84a4ce760b176b32873be6aa25112d1b64fd96c
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-23 13:17:08 +0700

    Update font.php

--- font.php
+++ font.php
@@ -6,12 +6,13 @@
         font-family: "MyFont1";
         src: url("baozi.ttf");
       }
-      body{
+      #font1{
         font-family: "MyFont1";
       }
     </style>
   </head>
   <body>
+    <h1 id="font1">ฟอนต์ของฉัน</h1>
     <h1>ฟอนต์ของฉัน</h1>
   </body>
 </html>
