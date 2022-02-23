9adb32d6de8649419737887277f8441a072bccf5
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-02-23 13:20:50 +0700

    Update font.php

--- font.php
+++ font.php
@@ -6,13 +6,20 @@
         font-family: "MyFont1";
         src: url("baozi.ttf");
       }
+      @font-face{
+        font-family: "MyFont2";
+        src: url("baozi.ttf");
+      }
       #font1{
         font-family: "MyFont1";
+      }
+      #font2{
+        font-family: "MyFont2";
       }
     </style>
   </head>
   <body>
-    <h1 id="font1">ฟอนต์ของฉัน</h1>
-    <h1>ฟอนต์ของฉัน</h1>
+    <h1 id="font1">นายยิ่งคุณ รอดทิม เลขที่ 1 ห้อง ม.4/14 เลขประจำตัว 88888</h1>
+    <h1 id="font2">นายยิ่งคุณ รอดทิม เลขที่ 1 ห้อง ม.4/14 เลขประจำตัว 88888</h1>
   </body>
 </html>
