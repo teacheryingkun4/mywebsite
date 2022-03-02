83ce4fd270d40a8bf4bd386cbc81d8ba0785d5bd
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-03-02 13:04:28 +0700

    Create table1.php

---
+++ table1.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td>คอลัมน์ที่ 1</td>
+        <td>คอลัมน์ที่ 2</td>
+        <td>คอลัมน์ที่ 3</td>
+      </tr>
+    </table>
+  </body>
+</html>
