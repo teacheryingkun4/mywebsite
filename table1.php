2be7228e378de619e9a0d53430d1bdfd4e6b74ae
teacheryingkun4 <97937648+teacheryingkun4@users.noreply.github.com>
2022-03-02 13:20:29 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -5,14 +5,16 @@
   <body>
     <table border="1">
       <tr>
-        <th>คอลัมน์ที่ 1</th>
-        <th>คอลัมน์ที่ 2</th>
-        <th>คอลัมน์ที่ 3</th>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
       </tr>
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
-        <td>คอลัมน์ที่ 3</td>
+        <td>1</td>
+        <td>ยิ่งคุณ รอดทิม</td>
+        <td><img src="https://i.pinimg.com/280x280_RS/94/e4/d1/94e4d1683b63958c29e2fa96a176f016.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/yunking.rodtim">Facebook</a></td>
       </tr>
     </table>
   </body>
